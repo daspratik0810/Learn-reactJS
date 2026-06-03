2ba98b2b76d5d57d0d0397644580a22e91d0361e
Pratik Das <daspratik0810@gmail.com>
2026-06-03 00:01:39 +0000

    bgcolor-part2

--- 04_bgChanger/src/App.jsx
+++ 04_bgChanger/src/App.jsx
@@ -4,13 +4,13 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("green")
 
   return (
-    <>
-      
-    </>
+    <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
+    </div>
   )
 }
 
