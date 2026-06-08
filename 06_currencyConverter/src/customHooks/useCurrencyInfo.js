5074d1762d6a042f3eef361b85375329eb3e1d08
Pratik Das <daspratik0810@gmail.com>
2026-06-08 22:49:07 +0000

    notes for custom hook

---
+++ 06_currencyConverter/src/customHooks/useCurrencyInfo.js
@@ -0,0 +1,4 @@
+//Mostly API data callings come with string format, it loks like an JSONobj but when we call and fetch, then it comes in strings
+//we can also do typeOf and see that it is string, so we need to handle the API calling,ie, convert the incoming data into JSON
+//By default in React, there is a React Query which bydefault handles API. 
+// React Query is a JavaScript library designed to simplify the complex task of data fetching and caching in React applications 
