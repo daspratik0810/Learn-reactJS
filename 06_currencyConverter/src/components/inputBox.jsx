dc0e63cedfd0e2d461831b75456f6c862c812fdd
Pratik Das <daspratik0810@gmail.com>
2026-06-09 23:09:24 +0000

    fixed the from and to title

--- 06_currencyConverter/src/components/inputBox.jsx
+++ 06_currencyConverter/src/components/inputBox.jsx
@@ -14,7 +14,7 @@ function InputBox({
    const amountInputId = useId()
 
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className} text-left`}>
             <div className="w-1/2">
                 <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
                     {label}
