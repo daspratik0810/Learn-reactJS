c55bf18e099e786ae79276844da881f7e6b33f34
Pratik Das <daspratik0810@gmail.com>
2026-06-13 00:18:00 +0000

    corrected themeMode useEffect and light or dark theme

--- 10_themeSwitcher/src/App.jsx
+++ 10_themeSwitcher/src/App.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ThemeProvider, ThemeContext } from "./context/theme";
+
 
 function App() {
-  return (
+  const [themeMode, setThemeMode] = useState("light")
+  const [lightTheme, setlightTheme] = useState()
+  const [darkTheme, setdarkTheme] = useState()
 
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+  
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  //works in every re-rendering
+  useEffect( () => {
+    document.querySelector("html").classList.remove("light","dark") //remove default light or dark in themeMode
+    document.querySelector("html").classList.add(themeMode) //add light or dark in themeMode
+
+  }, [themeMode])
+  //as soon as the user changes the themeMode value, this function will run and remove the default themeMode value and add a new themeMode value to it
+  //3 variables with a provider is passed onto components, where they can access these values
+  return (
+  <ThemeProvider value = {{themeMode, darkTheme, lightTheme}}>
+  
     <div className="flex flex-wrap min-h-screen items-center">
       <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
@@ -14,7 +38,8 @@ function App() {
         </div>
       </div>
     </div>
-
+  
+  </ThemeProvider>
   );
 }
 
