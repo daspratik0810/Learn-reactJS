333c5058d445ce529c0e14a0b64afa8beb96d1c2
Pratik Das <daspratik0810@gmail.com>
2026-06-13 01:16:29 +0000

    completed ThemeSwitcher

--- 10_themeSwitcher/src/App.jsx
+++ 10_themeSwitcher/src/App.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { ThemeProvider, ThemeContext } from "./context/theme";
-
+import Card from "./components/Card";
+import ThemeBtn from "./components/ThemeBtn";
 
 function App() {
   const [themeMode, setThemeMode] = useState("light")
-  const [lightTheme, setlightTheme] = useState()
-  const [darkTheme, setdarkTheme] = useState()
+
 
   const lightTheme = () => {
     setThemeMode("light")
@@ -23,6 +23,7 @@ function App() {
 
   }, [themeMode])
   //as soon as the user changes the themeMode value, this function will run and remove the default themeMode value and add a new themeMode value to it
+  
   //3 variables with a provider is passed onto components, where they can access these values
   return (
   <ThemeProvider value = {{themeMode, darkTheme, lightTheme}}>
@@ -30,11 +31,11 @@ function App() {
     <div className="flex flex-wrap min-h-screen items-center">
       <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/*themeBtn */}
+          <ThemeBtn />
         </div>
 
         <div className="w-full max-w-sm mx-auto">
-          {/* Card */}
+          <Card />
         </div>
       </div>
     </div>
